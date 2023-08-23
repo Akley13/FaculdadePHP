6aa126902ad62b7152db1cb018704550010f2376
Akley Alves <akleyalvescr@gmail.com>
2023-08-22 21:38:10 -0300

    updt

--- Projeto 01/index.php
+++ Projeto 01/index.php
@@ -27,7 +27,12 @@
             echo "<form action='validarLoginPOST.php' method='POST'>";
             echo "<h2>Informe e-mail e senha</h2>";
             echo "E-mail: <input type='text' name='email' required> <br> <br>";
-            echo "Senha: <input type='password' name='senha' required>";
+            echo "Senha: <input type='password' name='senha' required> <br> <br> ";
+            echo "<input type='submit' value='Enviar'>";
+            echo "<input type='button' value='Cadastrar'>";
+            echo "<input type='button' value='Cancelar'>";
+            echo "</form>";
+
 
         ?>
     </p>
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FaculdadePHP
  Utilizando HTML e PHP 
+
+Para abrir no navegador: http://localhost/faculdadePHP/Projeto%2001/
