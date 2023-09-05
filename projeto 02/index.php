131209cb98d2541295a3f963eafdb0c638db66f9
Akley Alves <akleyalvescr@gmail.com>
2023-09-05 15:34:34 -0300

    updt

---
+++ projeto 02/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRIMEIROS TESTES PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "TESTE DE AMBIENTE";
+        ?>
+    </h1>
+
+    <p>
+        <?php
+            $desenv = "VS CODE";
+            $teste = "XAMP";
+            echo "Ambiente de desenvolvimento: $desenv e $teste"
+        ?>
+    </p>
+
+</body>
+</html>
