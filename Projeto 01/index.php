8b513b02371103f18e9232fd1595d3189e386d32
Akley Alves <akleyalvescr@gmail.com>
2023-09-05 17:04:10 -0300

    UPDT

--- Projeto 01/index.php
+++ Projeto 01/index.php
@@ -3,22 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MENU | PHP</title>
+    <title>PRIMEIROS TESTES PHP</title>
 </head>
 <body>
-
     <h1>
         <?php
-            echo "MENU | PHP <hr>";
+            echo "TESTE DE AMBIENTE";
         ?>
     </h1>
 
     <p>
         <?php
-        echo "<a href='index.php'>Principal | </a>";
-        echo "<a href='login.php'>Login | </a>";
-        echo "<a href='cadastro.php'>Cadastro | </a>";
-        echo "<a href='sair.php'>Sair</a>";
+            $desenv = "VS CODE";
+            $teste = "XAMP";
+            echo "Ambiente de desenvolvimento: $desenv e $teste"
         ?>
     </p>
 
--- projeto 02/index.php
+++ projeto 02/index.php
@@ -3,20 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PRIMEIROS TESTES PHP</title>
+    <title>MENU | PHP</title>
 </head>
 <body>
+
     <h1>
         <?php
-            echo "TESTE DE AMBIENTE";
+            echo "MENU | PHP <hr>";
         ?>
     </h1>
 
     <p>
         <?php
-            $desenv = "VS CODE";
-            $teste = "XAMP";
-            echo "Ambiente de desenvolvimento: $desenv e $teste"
+        echo "<a href='index.php'>Principal | </a>";
+        echo "<a href='login.php'>Login | </a>";
+        echo "<a href='cadastro.php'>Cadastro | </a>";
+        echo "<a href='sair.php'>Sair</a>";
         ?>
     </p>
 
